57b71bc64f52ef09026177534b549259a12ae5d6
Sarthak Malik <malik@sarthak.ca>
2020-10-03 17:54:19 -0400

    Static assets as module type defs

---
+++ src/types/imports.d.ts
@@ -0,0 +1,10 @@
+/**
+ * Declare module resolution of custom types
+ */
+
+/// <reference types="react-scripts" />
+
+declare module '*.mp4';
+declare module '*.jpg';
+declare module '*.png';
+declare module '*.svg';
